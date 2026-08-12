51f5c3a1c74280eadb4a00b96a73bacedc5aebc0
Saikrishna <saikrishna580@gmail.com>
2026-08-12 16:11:35 +0530

    api doc added

---
+++ mongodb/master-data.js
@@ -0,0 +1,89 @@
+<mxfile host="app.diagrams.net">
+  <diagram name="Page-1" id="fQYlXHRrstufMsucMljE">
+    <mxGraphModel dx="1030" dy="561" grid="1" gridSize="10" guides="1" tooltips="1" connect="1" arrows="1" fold="1" page="1" pageScale="1" pageWidth="850" pageHeight="1100" math="0" shadow="0">
+      <root>
+        <mxCell id="0" />
+        <mxCell id="1" parent="0" />
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-1" parent="1" style="rounded=1;whiteSpace=wrap;html=1;" value="" vertex="1">
+          <mxGeometry height="240" width="160" x="280" y="80" as="geometry" />
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-3" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="375" y="170" as="geometry" />
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-4" parent="1" style="verticalLabelPosition=bottom;shadow=0;dashed=0;align=center;html=1;verticalAlign=top;strokeWidth=1;shape=mxgraph.mockup.containers.userFemale;strokeColor=#666666;strokeColor2=#008cff;" value="" vertex="1">
+          <mxGeometry height="100" width="100" x="50" y="140.5" as="geometry" />
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-5" edge="1" parent="1" source="IYkIUhZtwg3Rp9GHFw_G-4" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;entryX=-0.017;entryY=0.451;entryDx=0;entryDy=0;entryPerimeter=0;" target="IYkIUhZtwg3Rp9GHFw_G-1">
+          <mxGeometry relative="1" as="geometry">
+            <Array as="points">
+              <mxPoint x="277" y="191" />
+            </Array>
+          </mxGeometry>
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-8" edge="1" parent="1" source="IYkIUhZtwg3Rp9GHFw_G-6" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;" target="IYkIUhZtwg3Rp9GHFw_G-3">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-6" parent="1" style="rounded=0;whiteSpace=wrap;html=1;" value="&lt;b&gt;80&lt;/b&gt;" vertex="1">
+          <mxGeometry height="60" width="50" x="250" y="160.5" as="geometry" />
+        </mxCell>
+        <mxCell id="IYkIUhZtwg3Rp9GHFw_G-9" parent="1" style="text;html=1;whiteSpace=wrap;strokeColor=none;fillColor=none;align=center;verticalAlign=middle;rounded=0;" value="&lt;b&gt;80&lt;/b&gt;" vertex="1">
+          <mxGeometry height="30" width="60" x="370" y="140.5" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-1" parent="1" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#dae8fc;gradientColor=#7ea6e0;strokeColor=#6c8ebf;" value="&lt;b&gt;Bare Metal Server&lt;/b&gt;" vertex="1">
+          <mxGeometry height="80" width="240" x="560" y="240" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-2" parent="1" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#d5e8d4;gradientColor=#97d077;strokeColor=#82b366;" value="&lt;b&gt;Hypervisor&lt;/b&gt;" vertex="1">
+          <mxGeometry height="50" width="240" x="560" y="190" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-3" parent="1" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#ffcd28;gradientColor=#ffa500;strokeColor=#d79b00;" value="&lt;b&gt;VM&lt;/b&gt;" vertex="1">
+          <mxGeometry height="160" width="80" x="560" y="30" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-4" parent="1" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#f8cecc;gradientColor=#ea6b66;strokeColor=#b85450;" value="&lt;b&gt;VM&lt;/b&gt;" vertex="1">
+          <mxGeometry height="160" width="80" x="640" y="30" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-5" parent="1" style="rounded=0;whiteSpace=wrap;html=1;fillColor=#e6d0de;gradientColor=#d5739d;strokeColor=#996185;" value="&lt;b&gt;VM&lt;/b&gt;" vertex="1">
+          <mxGeometry height="160" width="80" x="720" y="30" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-6" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="575" y="50" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-7" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="655" y="50" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-8" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="735" y="50" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-9" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="580" y="129.5" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-10" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="660" y="129.5" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-11" parent="1" style="image;sketch=0;aspect=fixed;html=1;points=[];align=center;fontSize=12;image=img/lib/mscae/Docker.svg;" value="" vertex="1">
+          <mxGeometry height="41" width="50" x="735" y="129" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-12" parent="1" style="shape=cylinder3;whiteSpace=wrap;html=1;boundedLbl=1;backgroundOutline=1;size=15;" value="&lt;b&gt;MongoDB&lt;/b&gt;&lt;div&gt;&lt;b&gt;Server&lt;/b&gt;&lt;/div&gt;" vertex="1">
+          <mxGeometry height="80" width="70" x="480" y="470" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-14" edge="1" parent="1" source="8u0VsHRpZV4PplqS8_me-13" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;" target="8u0VsHRpZV4PplqS8_me-12">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-13" parent="1" style="ellipse;whiteSpace=wrap;html=1;" value="&lt;b&gt;mongod admins&lt;/b&gt;" vertex="1">
+          <mxGeometry height="50" width="120" x="215" y="485" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-15" parent="1" style="ellipse;whiteSpace=wrap;html=1;" value="&lt;b&gt;catalogue&lt;/b&gt;" vertex="1">
+          <mxGeometry height="50" width="120" x="215" y="570" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-16" edge="1" parent="1" source="8u0VsHRpZV4PplqS8_me-15" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;entryX=0;entryY=0.5;entryDx=0;entryDy=0;entryPerimeter=0;" target="8u0VsHRpZV4PplqS8_me-12">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-17" parent="1" style="sketch=0;outlineConnect=0;fontColor=#232F3E;gradientColor=none;fillColor=#8C4FFF;strokeColor=none;dashed=0;verticalLabelPosition=bottom;verticalAlign=top;align=center;html=1;fontSize=12;fontStyle=0;aspect=fixed;pointerEvents=1;shape=mxgraph.aws4.datazone_data_portal;" value="" vertex="1">
+          <mxGeometry height="78" width="78" x="476" y="300" as="geometry" />
+        </mxCell>
+        <mxCell id="8u0VsHRpZV4PplqS8_me-18" edge="1" parent="1" source="8u0VsHRpZV4PplqS8_me-17" style="edgeStyle=orthogonalEdgeStyle;rounded=0;orthogonalLoop=1;jettySize=auto;html=1;entryX=0.5;entryY=0;entryDx=0;entryDy=0;entryPerimeter=0;" target="8u0VsHRpZV4PplqS8_me-12">
+          <mxGeometry relative="1" as="geometry" />
+        </mxCell>
+      </root>
+    </mxGraphModel>
+  </diagram>
+</mxfile>
